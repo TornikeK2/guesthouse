b9a6aca5bcbda1037206cf44b0657dacdfd5a97f
TornikeK2 <147658406+TornikeK2@users.noreply.github.com>
2024-05-23 00:24:25 +0400

    Add files via upload

---
+++ P/hotel/contactform2/contactform2/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('full-name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        data = {
+            'name' : name,
+            'email' : email,
+            'subject' : subject,
+            'message' : message
+        }
+        message = '''
+        New message: {}
+
+        From: {}    
+        '''.format(data['message'], data['email'])
+        send_mail(data['subject'], message, '', ['kacharavatornike178@gmail.com'])
+
+        
+    return render(request, 'contactform2/index.html', {})
